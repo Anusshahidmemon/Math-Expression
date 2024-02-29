7c5f6bcc655e009fb91ee3951f1ba87b5f393091
Anusshahidmemon <shahidanus21@gmail.com>
2024-02-29 03:27:27 -0800

    math expression

---
+++ app.js
@@ -0,0 +1,24 @@
+// let num = 10
+// ++num
+// document.write("the value of ++num  is " + num + "<br>")
+// num++
+// document.write("the value of num++ is " + num + "<br>")
+// --num
+// document.write("the value of --num  is " + num + "<br>")
+// num--
+// document.write("the value of num-- is " + num + "<br>")
+
+// let a =2 ,b=1 
+// let result = --a - --b + ++b + b--
+// document.write(a + "<br>")//1
+//  document.write(b + "<br>")//0
+//  document.write(result)//1
+
+//  let name = prompt("Enter the user name")
+//  alert("Welcome" + " " + name)
+let number =prompt("Enter a number")
+ for(let i=1; i<=10; i++ ){
+    console.log(number * i)
+
+ }
+ 
